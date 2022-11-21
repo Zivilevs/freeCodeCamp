d72bc62e2ae0c794b53bdad4ac3d7154f5b3f8d3
Zivile V. Silveira <54817312+Zivilevs@users.noreply.github.com>
2022-11-20 22:17:21 -0300

    Add files via upload

--- insert_data.sh
+++ insert_data.sh
@@ -14,25 +14,24 @@ do
 # add winner team 
 if [[ $WINNER != "winner" ]]
 then
-        WINNER_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$WINNER'")
-        if [[ -z $WINNER_ID ]]
+        TEAM_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$WINNER'")
+        if [[ -z $TEAM_ID ]]
         then
-                $($PSQL "INSERT INTO teams(name) values('$WINNER')")
-                WINNER_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$WINNER'")
+		INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) values('$WINNER')")
         fi
 # add opponent team
-        OPPONENT_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$OPPONENT'")
-        if [[ -z $OPPONENT_ID ]]
+        TEAM_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$OPPONENT'")
+        if [[ -z $TEAM_ID ]]
         then
-                $($PSQL "INSERT INTO teams(name) values('$OPPONENT')")
-                OPPONENT_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$OPPONENT'")
+                INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) values('$OPPONENT')")
         fi
 # add other columns to games table
+        WINNER_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$WINNER'")
+        OPPONENT_ID=$($PSQL "SELECT team_id FROM TEAMS WHERE name='$OPPONENT'")
         GAME_ID=$($PSQL "select game_id from games where winner_id=$WINNER_ID and opponent_id=$OPPONENT_ID and round='$ROUND' and year=$YEAR")
         if [[ -z $GAME_ID ]]
         then
-                echo $GAME_ID
-                $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $W_GOALS, $O_GOALS)")
+                INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $W_GOALS, $O_GOALS)")
         fi
 fi
 done
